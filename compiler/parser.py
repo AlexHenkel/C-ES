1ad517d7ffbaf684b0faad8300ab325133923df2
Alejandro Henkel <alehenkel17@gmail.com>
2018-03-06 16:07:15 -0600

    Fix reduce/reduce conflicts with recursion

--- compiler/parser.py
+++ compiler/parser.py
@@ -17,33 +17,23 @@ def p_variables_opt(p):
 
 def p_main_func(p):
     '''main_func : empty
-                 | function main_func_rec'''
-
-
-def p_main_func_rec(p):
-    '''main_func_rec : empty
-                     | main_func'''
-
+                 | function main_func'''
 
 # Block
+
+
 def p_block(p):
     'block : "{" block_stm_opt "}"'
 
 
 def p_block_stm_opt(p):
     '''block_stm_opt : empty
-                     | statement block_stm_rec'''
-
-
-def p_block_stm_rec(p):
-    '''block_stm_rec : empty
-                     | block_stm_opt'''
+                     | statement block_stm_opt'''
 
 
 # Statement
 def p_statement(p):
-    '''statement : empty
-                 | assignation
+    '''statement : assignation
                  | condition
                  | iteration
                  | function_call'''
@@ -56,7 +46,7 @@ def p_variables(p):
 
 
 def p_var_opts(p):
-    '''var_opts : base_type 
+    '''var_opts : base_type
                 | ARRAY FROM base_type FROM id_or_number'''
 
 
@@ -68,9 +58,8 @@ def p_var_id_rec(p):
     '''var_id_rec : empty
                   | "," var_id'''
 
+
 # Assignation
-
-
 def p_assignation(p):
     'assignation : ID "=" expression'
 
@@ -85,21 +74,16 @@ def p_cond_if(p):
 
 
 def p_cond_else_if_opt(p):
-    '''cond_else_if_opt : empty 
-                        | cond_else_if cond_else_if_rec'''
+    '''cond_else_if_opt : empty
+                        | cond_else_if cond_else_if_opt'''
 
 
 def p_cond_else_if(p):
     'cond_else_if : OR IF HAPPENS "(" expression ")" DO block'
 
 
-def p_cond_else_if_rec(p):
-    '''cond_else_if_rec : empty 
-                        | cond_else_if_opt'''
-
-
 def p_cond_else_opt(p):
-    '''cond_else_opt : empty 
+    '''cond_else_opt : empty
                      | ELSE HAPPENS block'''
 
 
@@ -119,7 +103,7 @@ def p_function(p):
 
 
 def p_function_type(p):
-    '''function_type : empty 
+    '''function_type : empty
                      | type'''
 
 
@@ -128,29 +112,23 @@ def p_function_params(p):
 
 
 def p_function_params_rec(p):
-    '''function_params_rec : empty 
+    '''function_params_rec : empty
                            | function_params'''
 
 
 def p_function_stm(p):
-    '''function_stm : empty 
-                    | statement function_stm_rec'''
-
-
-def p_function_stm_rec(p):
-    '''function_stm_rec : empty 
-                        | function_stm'''
+    '''function_stm : empty
+                    | statement function_stm'''
 
 
 def p_function_return(p):
-    '''function_return : empty 
+    '''function_return : empty
                        | RETURN expression'''
 
 
 # Function call
 def p_function_call(p):
-    '''function_call : empty 
-                     | read
+    '''function_call : read
                      | print
                      | local_function
                      | list_push
@@ -173,7 +151,7 @@ def p_expr_params(p):
 
 
 def p_expr_params_rec(p):
-    '''expr_params_rec : empty 
+    '''expr_params_rec : empty
                        | "," expr_params'''
 
 
@@ -202,42 +180,38 @@ def p_random(p):
 
 # Expression
 def p_expression(p):
-    '''expression : exp_comp
-                  | expression_opt'''
+    'expression : exp_comp expression_opt'
 
 
 def p_expression_opt(p):
-    '''expression_opt : empty 
+    '''expression_opt : empty
                        | binary_operators exp_comp'''
 
 
 def p_exp_comp(p):
-    '''exp_comp : exp_add 
-                | exp_comp_opt'''
+    'exp_comp : exp_add exp_comp_opt'
 
 
 def p_exp_comp_opt(p):
-    '''exp_comp_opt : empty 
+    '''exp_comp_opt : empty
                     | comp_operators exp_add'''
 
 
 def p_exp_add(p):
-    '''exp_add : exp_multiply 
-               | exp_add_opt'''
+    'exp_add : exp_multiply exp_add_opt'
 
 
 def p_exp_add_opt(p):
-    '''exp_add_opt : empty 
+    '''exp_add_opt : empty
                    | add_operators exp_multiply'''
 
 
 def p_exp_multiply(p):
-    '''exp_multiply : term 
-                    | exp_multiply_opt'''
+    'exp_multiply : term exp_multiply_opt'
 
 
 def p_exp_multiply_opt(p):
-    '''exp_multiply_opt : empty 
+    '''exp_multiply_opt : empty
                         | multiply_operators term'''
 
 
@@ -256,12 +230,12 @@ def p_term_body(p):
 
 
 def p_term_body_opt(p):
-    '''term_body_opt : empty 
+    '''term_body_opt : empty
                      | add_operators'''
 
 
 def p_term_body_types(p):
-    '''term_body_types : ID 
+    '''term_body_types : ID
                      | CONST_I
                      | CONST_F
                      | CONST_S
@@ -331,11 +305,11 @@ def p_empty(p):
 # Build the parser
 parser = yacc.yacc()
 
-# # Open and read input
-# f = open("input.txt", "r")
-# s = f.read()
-# # Parse text if found
-# if s:
-#     result = parser.parse(s)
-# # Program finished
-# print("Program finished")
+# Open and read input
+f = open("input.txt", "r")
+s = f.read()
+# Parse text if found
+if s:
+    result = parser.parse(s)
+# Program finished
+print("Program finished")
