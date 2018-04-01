1951fac79018de2bd9ea2947018b829da51b3b9d
Fernando López Martínez <fer.lopez94@outlook.com>
2018-03-31 21:51:38 -0600

    Change lex import order

--- compiler/parser.py
+++ compiler/parser.py
@@ -2,8 +2,8 @@ import glob
 from optparse import OptionParser
 from ply import yacc
 #import ply.yacc as yacc
-from lex import tokens
 from random import randint
+from lex import tokens
 from semantic_cube import types, get_semantic_result
 from memory import get_memory_address
 from errors import *
