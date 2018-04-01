5d2dd6d27155e21d33db6955def681ebb55593ce
Fernando López Martínez <fer.lopez94@outlook.com>
2018-03-31 22:26:30 -0600

    Remove ply using pip

--- compiler/lex.py
+++ compiler/lex.py
@@ -1,5 +1,4 @@
 import ply.lex as lex
-#from ply import lex
 
 reserved = {
     'a': 'TO',
--- compiler/parser.py
+++ compiler/parser.py
@@ -1,6 +1,5 @@
 import glob
 from optparse import OptionParser
-#from ply import yacc
 import ply.yacc as yacc
 from random import randint
 from semantic_cube import types, get_semantic_result
